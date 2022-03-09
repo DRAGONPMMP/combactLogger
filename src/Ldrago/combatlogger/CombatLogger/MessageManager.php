c8efffcaa7eedd8012b7c7f8838ee633cefd7104
DRAGONPMMP <93898371+DRAGONPMMP@users.noreply.github.com>
2022-03-09 15:04:40 +0530

    Update MessageManager.php

--- src/Ldrago/combatlogger/CombatLogger/MessageManager.php
+++ src/Ldrago/combatlogger/CombatLogger/MessageManager.php
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  */
 
-namespace jacknoordhuis\combatlogger;
+namespace Ldrago\combatlogger;
 
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\utils\TextFormat;
